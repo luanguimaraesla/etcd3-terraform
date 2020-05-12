e34d876a4f8b8996b3a08494d84c2377b901ef78
Luan Guimarães <luan.guimaraes@tfgco.com>
2020-05-12 15:56:08 -0300

    defines variables for the module

--- variables.tf
+++ variables.tf
@@ -1,33 +1,35 @@
-provider "aws" {
-  region = "eu-central-1"
-}
-
 variable "instance_type" {
   default = "m5.large"
-}
-
-variable "region" {
-  default = "eu-central-1"
+  description = "AWS instance type"
 }
 
 variable "azs" {
-  default = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+  default = []
+  type = list
+  description = "List of AWS availability zones. Make sure you have more than three zones for HA setup"
 }
 
-variable "environment" {
-  default = "staging"
+variable "tags" {
+  default = {}
+  type = map
+  description = "AWS tags which will be present in all cloud resources"
 }
 
 variable "role" {
-  default = "etcd3-test"
+  default = "etcd3"
+  type = string
+  description = "AWS role name for ETCD usage"
 }
 
 variable "ami" {
   default = "ami-1f62c270"
+  type = string
+  description = "AWS AMI ID to use"
 }
 
-variable "vpc_cidr" {
-  default = "10.200.0.0/16"
+variable "vpc_id" {
+  type = string
+  description = "AWS VPC to create all resources"
 }
 
 variable "dns" {
@@ -39,13 +41,16 @@ variable "dns" {
 }
 
 variable "root_key_pair_public_key" {
+  type = string
+  description = "AWS key pair public key to attach to instances"
 }
 
 variable "cluster_size" {
   default = 3
+  description = "ETCD cluster size. Maximum: 5"
 }
 
 variable "ntp_host" {
   default = "0.europe.pool.ntp.org"
+  description = "NTP host you want to use within etcd instances"
 }
-
