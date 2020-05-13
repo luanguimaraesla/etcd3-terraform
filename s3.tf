e199e3408bb73058108b158ade8bf7092311cc7d
Luan Guimarães <luan.guimaraes@tfgco.com>
2020-05-13 20:26:50 -0300

    fix wrong file path

--- s3.tf
+++ s3.tf
@@ -1,8 +1,8 @@
 resource "aws_s3_bucket_object" "etcd3-bootstrap-linux-amd64" {
   bucket       = local.aws_s3_bucket_name 
   key          = local.aws_s3_bucket_etcd_bootstrap_key
-  source       = "files/etcd3-bootstrap-linux-amd64"
-  etag         = filemd5("files/etcd3-bootstrap-linux-amd64")
+  source       = "${path.module}/files/etcd3-bootstrap-linux-amd64"
+  etag         = filemd5("$path.module}/files/etcd3-bootstrap-linux-amd64")
   acl          = "public-read"
   content_type = "application/octet-stream"
 }
